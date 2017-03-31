9a6f9ed753d939ba17dd7a4ac19849d5fde3be31
WuXueqian <wuxueqian2010@hotmail.com>
2017-03-31 16:37:10 +0800

    Fix mongoose promise warning

--- app/models/user.model.ts
+++ app/models/user.model.ts
@@ -65,6 +65,10 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: UserRole,
+        default: UserRole.subscriber
     }
 })
 
